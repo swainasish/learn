f4e3ef5d441a700e9b570fc633d785d6abd86700
asis <asis@pop-os.localdomain>
2023-02-26 00:01:21 +0530

    a

--- prompt_101/docusaurus.config.js
+++ prompt_101/docusaurus.config.js
@@ -6,13 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://github.com',
+  baseUrl: '/learn/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'swainasish', // Usually your GitHub org/user name.
+  projectName: 'learn', // Usually your repo name.
+  deploymentBranch: 'gh-pages'
 
   presets: [
     [
