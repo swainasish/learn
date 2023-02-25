364563aa89763610e3ad7c95d00a696ebbadec93
asis <asis@pop-os.localdomain>
2023-02-26 00:18:56 +0530

    update

--- prompt_101/docusaurus.config.js
+++ prompt_101/docusaurus.config.js
@@ -13,7 +13,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   favicon: 'img/favicon.ico',
   organizationName: 'swainasish', // Usually your GitHub org/user name.
   projectName: 'learn', // Usually your repo name.
-  deploymentBranch: 'gh-pages'
 
   presets: [
     [
